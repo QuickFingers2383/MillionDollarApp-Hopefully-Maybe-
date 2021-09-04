caab3b53a5ca390f61349095a6a0a262a245e162
David Pidugu <dpidu001@gmail.com>
2021-09-04 18:59:17 -0400

    Navigation Bar

--- MillionDollarApp(hopefully(maybe))/MillionDollarApp(hopefully(maybe))/ContentView.swift
+++ MillionDollarApp(hopefully(maybe))/MillionDollarApp(hopefully(maybe))/ContentView.swift
@@ -17,19 +17,27 @@ struct ContentView: View {
     private var items: FetchedResults<Item>
 
     var body: some View {
-        List {
-            ForEach(items) { item in
-                Text("Item at \(item.timestamp!, formatter: itemFormatter)")
+        NavigationView {
+            List {
+                ForEach(items) { item in
+                    Text("Item at \(item.timestamp!, formatter: itemFormatter)")
+                }
+                .onDelete(perform: deleteItems)
             }
-            .onDelete(perform: deleteItems)
-        }
-        .toolbar {
-            #if os(iOS)
-            EditButton()
-            #endif
-
-            Button(action: addItem) {
-                Label("Add Item", systemImage: "plus")
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .accentColor(Color("accent"))
+            .background(Color("accent"))
+            .navigationViewStyle(StackNavigationViewStyle())
+            .navigationBarTitle("App")
+            .navigationBarHidden(false)
+            .toolbar {
+                #if os(iOS)
+                EditButton()
+                #endif
+                
+                Button(action: addItem) {
+                    Label("Add Item", systemImage: "plus")
+                }
             }
         }
     }
--- MillionDollarApp(hopefully(maybe))/MillionDollarApp(hopefully(maybe))/MillionDollarApp_hopefully_maybe__App.swift
+++ MillionDollarApp(hopefully(maybe))/MillionDollarApp(hopefully(maybe))/MillionDollarApp_hopefully_maybe__App.swift
@@ -6,11 +6,29 @@
 //
 
 import SwiftUI
+import Foundation
 
 @main
 struct MillionDollarApp_hopefully_maybe__App: App {
     let persistenceController = PersistenceController.shared
-
+    
+    init() {
+           UINavigationBar.appearance().backgroundColor = UIColor.clear
+           UINavigationBar.appearance().isTranslucent = true
+           UINavigationBar.appearance().titleTextAttributes = [
+               .foregroundColor: UIColor(named: "accent")!
+           ]
+           UINavigationBar.appearance().largeTitleTextAttributes = [
+               .foregroundColor: UIColor(named: "accent")!
+           ]
+           UINavigationBar.appearance().tintColor = UIColor(named: "accent")
+           UINavigationBar.appearance().barTintColor = UIColor(named: "accent")
+           UITabBar.appearance().backgroundImage = UIImage()
+           UITabBar.appearance().isTranslucent = true
+           UITabBar.appearance().backgroundColor = UIColor.clear
+           UITabBar.appearance().tintColor = UIColor(named: "accent")
+       }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
