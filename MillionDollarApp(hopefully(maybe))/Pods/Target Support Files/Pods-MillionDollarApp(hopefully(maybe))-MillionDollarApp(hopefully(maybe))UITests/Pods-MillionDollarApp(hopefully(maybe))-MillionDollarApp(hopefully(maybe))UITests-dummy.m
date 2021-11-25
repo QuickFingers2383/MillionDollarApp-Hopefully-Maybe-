9676c1a9f01194043fc15f50c0655c16af70da47
John Wang <55559153+Millenium-Falcon-NOT-Garbage@users.noreply.github.com>
2021-11-25 17:38:02 -0500

    Delete MillionDollarApp(hopefully(maybe)) directory

--- MillionDollarApp(hopefully(maybe))/Pods/Target Support Files/Pods-MillionDollarApp(hopefully(maybe))-MillionDollarApp(hopefully(maybe))UITests/Pods-MillionDollarApp(hopefully(maybe))-MillionDollarApp(hopefully(maybe))UITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_MillionDollarApp_hopefully_maybe___MillionDollarApp_hopefully_maybe__UITests : NSObject
-@end
-@implementation PodsDummy_Pods_MillionDollarApp_hopefully_maybe___MillionDollarApp_hopefully_maybe__UITests
-@end
